b0664451478d8704458af55919bf7484e2d35caa
tristankells <tristankells@gmail.com>
2018-06-19 15:55:45 +1200

    Dropdown for consultant client updates live added.

--- functions.php
+++ functions.php
@@ -12,22 +12,13 @@ function addClient($name)
 
 function addConsultant($name, $clients)
 {
-    $clientNames = array();
-
-
-    if ($clients->num_rows > 0) {
-        while ($row = $clients->fetch_assoc()) {
-            array_push($clientNames, $row['clientName']);
-        }
-    }
-
     echo "<tr>";
     echo "<td id='consultantName' colspan='2'>" . $name . "</td>";
     for ($x = 0; $x < 10; $x++) {
         echo "<td><select class='form-control' id='clientdropdown'><option></option>";
-        if (sizeof($clientNames) > 0 ) {
-            for ($i = 0; $i < sizeof($clientNames);$i++) {
-                echo "<option>" . $clientNames[$i]  . "</option>";
+        if (sizeof($clients) > 0 ) {
+            for ($i = 0; $i < sizeof($clients);$i++) {
+                echo "<option>" . $clients[$i]  . "</option>";
             }
         }
         echo "</td>";
