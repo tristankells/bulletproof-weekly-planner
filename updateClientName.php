10be048b42a6ff3680c0c2328b2aa1cf3fae18d2
tristankells <tristankells@gmail.com>
2018-07-07 21:26:30 +1200

    Made names for clients and consultants editable

---
+++ updateClientName.php
@@ -0,0 +1,15 @@
+<?php
+
+//Required to retrieve $conn variable used to connect the application database
+require_once 'database.php'
+;
+//Store the consultant ID, client name and the day/time of the client allocation
+$clientID = $_POST["clientID"];
+$clientName = $_POST["clientName"];
+echo ($clientName);
+
+//Query to update a consultatns client allocation for a specific day/time
+$query = "UPDATE clients SET ClientName = '$clientName' WHERE ClientID = '$clientID'";
+
+//Run query on connection
+$result = $conn->query($query);
---
+++ updateConsultantName.php
@@ -0,0 +1,14 @@
+<?php
+
+//Required to retrieve $conn variable used to connect the application database
+require_once 'database.php'
+;
+//Store the consultant ID, client name and the day/time of the client allocation
+$consultantID = $_POST["consultantID"];
+$consultantName = $_POST["consultantName"];
+
+//Query to update a consultatns client allocation for a specific day/time
+$query = "UPDATE consultants SET ConsultantName = '$consultantName' WHERE ConsultantID = '$consultantID'";
+
+//Run query on connection
+$result = $conn->query($query);
