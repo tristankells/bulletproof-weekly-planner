c7e19d5fed412a0c58a556451e73abc8551c9014
tristankells <tristankells@gmail.com>
2018-07-17 18:37:11 +1200

    connected the login screen to the login.php

--- login.php
+++ login.php
@@ -4,16 +4,14 @@ require_once 'backend/database.php';
 
 // Define varaible and initilaise with empty values
 $username = $password = "";
-$msg = "";
 
 // Processing form data when form is submitted
+
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 	// Check if username or password haven't been entered
 	if (empty(trim($_POST['username'])) || empty(trim($_POST['password']))) {
-        $msg = urlencode("Please fill in all the fields. ");
-		header("Location: ./index.php?msg=" . $msg);
-		echo($username .  $password);
+        echo("Please fill in all the fields. ");
         exit();
     } else { 
 		$username = trim($_POST['username']);
@@ -24,44 +22,36 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 		//Check if query succesful
         if ($passwordCheckSql = mysqli_query($conn, $passwordCheckQuery)) {
-            $numOfuser_profile = mysqli_num_rows($passwordCheckSql);
+			$numOfuser_profile = mysqli_num_rows($passwordCheckSql);
+			
+			//Check if any user match that username
             if ($numOfuser_profile == 0) {
-                $msg = urlencode("No user found");
-                header("Location: ./index.php?msg=" . $msg);
+                echo("No user found");
                 exit();
-            } else if ($numOfuser_profile == 1) {
+			} 
+			
+			//Check that one user matches username
+			else  {
                 $user_profilePassword = "";
                 while ($row = mysqli_fetch_assoc($passwordCheckSql)) {
                     $user_profilePassword = $row['login_password'];
                 }
                 if (strcmp($passwordHash, $user_profilePassword)) {
-                    $msg = urlencode("Incorrect password" . $passwordHash . "    " . $user_profilePassword);
-                    header("Location: ./index.php?msg=" . $msg);
+                    echo("Incorrect password" . $passwordHash . "    " . $user_profilePassword);
                     exit();
-                } else {
-					$msg = urlencode("Correct Password");
-					
+                } else {					
 					//Save username to session
 					session_start();
 					$_SESSION['username'] = $username;
-
-                    header("Location: ./index.html");
-                    exit();
+					echo('success');
                 }
-            } else {
-                $msg = urlencode("Duplicate username found");
-                header("Location: ./index.php?msg=" . $msg);
-                exit();
-            }
+            } 
 
             mysqli_free_result($passwordCheckSql);
         } else {
-            $msg = urlencode("password checking query error");
-            header("Location: ./index.php?msg=" . $msg);
+            echo("password checking query error");
             exit();
         }
-        $msg = urlencode("success");
-        header("Location: ./index.php?msg=" . $msg);
-        exit();
     }
 }
+
