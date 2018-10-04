1d77ee74764aaf83d5db6bab946bd39a9d153a14
tristankells <tristankells@gmail.com>
2018-10-04 14:17:05 +1300

    Updated the sql script

--- sql/databaseScript.sql
+++ sql/databaseScript.sql
@@ -37,22 +37,37 @@ CREATE TABLE allocation (
     PRIMARY KEY (consultant_id, allocation_slot, date_created)
 );
 
-CREATE TABLE monthly_allocation (
-    consultant_id       bigint NOT NULL REFERENCES consultant(id),
-    client_id           bigint  REFERENCES client(id),
-    allocation_slot     int NOT NULL,
-    time_allocated      timestamp NOT NULL default current_timestamp ON UPDATE current_timestamp,
-    PRIMARY KEY (consultant_id, allocation_slot, client_id)
-);
 
-CREATE TABLE user_profile (
-id   	        bigint NOT NULL AUTO_INCREMENT,
-first_name      varchar(30) NOT NULL,
-last_name       varchar(30),
-email           varchar(50) NOT NULL UNIQUE,  
-login_password  char(60) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL, 
-PRIMARY KEY (id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `user_profile` (
+  `id` bigint(20) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `login_password` char(60) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `external_type` varchar(16) NOT NULL,
+  `external_id` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `user_profile` (`id`, `first_name`, `last_name`, `email`, `login_password`, `external_type`, `external_id`) VALUES
+(1, 'Junha', 'Yu', 'test@gmail.com', '$2y$10$h61qCV9myUMxYD6VcWI2IOq63Okl7ioDmMM64/ez7rAoIwrz6HQEe', '', ''),
+(2, 'Junha', 'Yu', 'junehayu@gmail.com', '', 'google', '109571180129366142465');
+
+
+ALTER TABLE `user_profile`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+
+ALTER TABLE `user_profile`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+
 
 
 create TABLE membership (
--- sql/user_profile.sql
+++ sql/user_profile.sql
@@ -1,32 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 30, 2018 at 09:51 PM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `glance`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_profile`
---
 
 CREATE TABLE `user_profile` (
   `id` bigint(20) NOT NULL,
@@ -38,35 +16,17 @@ CREATE TABLE `user_profile` (
   `external_id` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user_profile`
---
+
 
 INSERT INTO `user_profile` (`id`, `first_name`, `last_name`, `email`, `login_password`, `external_type`, `external_id`) VALUES
 (1, 'Junha', 'Yu', 'test@gmail.com', '$2y$10$h61qCV9myUMxYD6VcWI2IOq63Okl7ioDmMM64/ez7rAoIwrz6HQEe', '', ''),
 (2, 'Junha', 'Yu', 'junehayu@gmail.com', '', 'google', '109571180129366142465');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `user_profile`
---
 ALTER TABLE `user_profile`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `user_profile`
---
 ALTER TABLE `user_profile`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
